b9325be46a3851ce1052fa812a82d105ea483703
Dmitriyrus99 <Dmitriyrus99@gmail.com>
2025-06-10 01:43:34 +0300

    Add auto site creation and persistent volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,29 @@
 set -e
 
 BENCH_FOLDER="${BENCH_FOLDER:-frappe-bench}"
+SITE_NAME="${SITE_NAME:-test_site}"
+ADMIN_PWD="${ADMIN_PWD:-admin}"
+DB_ROOT_PWD="${DB_ROOT_PWD:-""}"
 
 if [ ! -d "$BENCH_FOLDER" ]; then
     /bootstrap.sh
 fi
 
 cd "$BENCH_FOLDER"
-bench use dev.localhost
+
+if [ ! -f "/home/frappe/frappe-bench/sites/${SITE_NAME}/site_config.json" ]; then
+    echo ">> Creating Frappe site ${SITE_NAME}"
+    bench new-site "$SITE_NAME" \
+         --admin-password "$ADMIN_PWD" \
+         --mariadb-root-password "$DB_ROOT_PWD" \
+         --install-app erpnext
+fi
+
+bench use "$SITE_NAME"
 
 if [[ "$1" == "pytest" ]]; then
     shift
-    bench --site "${SITE_NAME:-dev.localhost}" run-tests --app ferum_customs \
+    bench --site "$SITE_NAME" run-tests --app ferum_customs \
         --junit-xml="${JUNIT_XML:-/app/reports/bench-results.xml}" "$@"
 else
     exec "$@"
